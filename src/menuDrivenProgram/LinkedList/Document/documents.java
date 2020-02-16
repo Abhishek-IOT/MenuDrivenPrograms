2141293ca3c75f0d4a0eeff75d70419df3cddbd0
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:53:02 +0530

    added the fields coverletter and cv and created getter method for it

--- src/menuDrivenProgram/LinkedList/Document/documents.java
+++ src/menuDrivenProgram/LinkedList/Document/documents.java
@@ -2,6 +2,16 @@ package menuDrivenProgram.LinkedList.Document;
 
 public class documents {
     private Page page = null;
+    private documents CV = null;
+    private documents coverletter = null;
+
+    public documents getCV() {
+        return CV;
+    }
+
+    public documents getCoverletter() {
+        return coverletter;
+    }
 
     public documents(Page page) {
         this.page = page;
