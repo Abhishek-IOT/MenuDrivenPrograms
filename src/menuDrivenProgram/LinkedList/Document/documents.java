fe6da84efe1588a7522d727a18de3a15e320b5e7
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:28:40 +0530

    created the document class and made a Page fields and made getter setter for it

---
+++ src/menuDrivenProgram/LinkedList/Document/documents.java
@@ -0,0 +1,13 @@
+package menuDrivenProgram.LinkedList.Document;
+
+public class documents {
+    private Page page = null;
+
+    public documents(Page page) {
+        this.page = page;
+    }
+
+    public Page getPage() {
+        return page;
+    }
+}
