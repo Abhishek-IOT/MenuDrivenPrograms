194fb25f5b44e95c639bb527823a9afb721ee470
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:18:18 +0530

    created getters and setters for page no and page details

--- src/menuDrivenProgram/LinkedList/Document/Page.java
+++ src/menuDrivenProgram/LinkedList/Document/Page.java
@@ -4,8 +4,24 @@ public class Page {
     private int pageNo=0;
     private String pageDetails="";
 
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public String getPageDetails() {
+        return pageDetails;
+    }
+
     public Page(int pageNo, String pageDetails) {
         this.pageNo = pageNo;
+        this.pageDetails = pageDetails;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public void setPageDetails(String pageDetails) {
         this.pageDetails = pageDetails;
     }
 }
