4dabf5352fb1c141239aed53fe2093b3cfac62fe
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:41:14 +0530

    overwritten the ADT methods

--- src/menuDrivenProgram/LinkedList/Defination/defination.java
+++ src/menuDrivenProgram/LinkedList/Defination/defination.java
@@ -1,2 +1,28 @@
 package menuDrivenProgram.LinkedList.Defination;
 
+import menuDrivenProgram.LinkedList.ADT.LinkedListADT;
+
+public class defination<documents> implements LinkedListADT<documents> {
+
+    @Override
+    public void add(documents data) {
+
+    }
+
+    @Override
+    public void remove(documents data) {
+
+    }
+
+    @Override
+    public int search(documents data) {
+        return 0;
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+
+}
