6ae77adf2923af0c1b392dcdbc1384068a9dff65
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:16:37 +0530

    created a Page class and made a constructor for fields page no and page details

---
+++ src/menuDrivenProgram/LinkedList/Document/Page.java
@@ -0,0 +1,11 @@
+package menuDrivenProgram.LinkedList.Document;
+
+public class Page {
+    private int pageNo=0;
+    private String pageDetails="";
+
+    public Page(int pageNo, String pageDetails) {
+        this.pageNo = pageNo;
+        this.pageDetails = pageDetails;
+    }
+}
