6090e698930957bda198de9da6463ff28d7f767c
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-16 12:39:17 +0530

    made the interface of type documents

--- src/menuDrivenProgram/LinkedList/ADT/LinkedListADT.java
+++ src/menuDrivenProgram/LinkedList/ADT/LinkedListADT.java
@@ -1,10 +1,15 @@
 package menuDrivenProgram.LinkedList.ADT;
 
-public interface LinkedListADT<document> {
+public interface LinkedListADT<documents> {
 
 
+    void add(documents data);
 
+    void remove(documents data);
 
+    int search(documents data);
+
+    void print();
 
 
 
---
+++ src/menuDrivenProgram/LinkedList/Defination/defination.java
@@ -0,0 +1,2 @@
+package menuDrivenProgram.LinkedList.Defination;
+
