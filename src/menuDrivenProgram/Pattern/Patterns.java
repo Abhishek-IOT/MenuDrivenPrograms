04bc91557119369ea8758d1882066936abbe9c22
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-23 23:48:26 +0530

    comments

--- src/menuDrivenProgram/Pattern/Patterns.java
+++ src/menuDrivenProgram/Pattern/Patterns.java
@@ -15,12 +15,12 @@ public class Patterns {
     public void pyramidPattern(int size) {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size - i; j++) {
-                System.out.print(" ");
+                System.out.print(" ");// for the spaces
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print("* ");// for printing the *
             }
-            System.out.println(" ");
+            System.out.println(" ");//for having the space between the *
         }
     }
 
