4e917c2db8d063207852797b71efe9e9e83f13b9
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-23 23:47:11 +0530

    method the program for the pyramid pattern

--- src/menuDrivenProgram/Pattern/Main.java
+++ src/menuDrivenProgram/Pattern/Main.java
@@ -3,6 +3,7 @@ package menuDrivenProgram.Pattern;
 public class Main {
     public static void main(String[] args) {
         Patterns patterns = new Patterns();
-        patterns.RightPattern(5);
+
+        patterns.pyramidPattern(5);
     }
 }
--- src/menuDrivenProgram/Pattern/Patterns.java
+++ src/menuDrivenProgram/Pattern/Patterns.java
@@ -10,8 +10,18 @@ public class Patterns {
             }
             System.out.println();
         }
+    }
 
-
+    public void pyramidPattern(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
     }
 
 
