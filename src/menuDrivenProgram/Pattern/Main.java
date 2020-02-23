dff4371fdd4adecd0dd25a205ae947e459546dc6
Abhishek-IOT <abhishek.srivastav_iot18@gla.ac.in>
2020-02-23 23:28:45 +0530

    created the right pyarmid pattern and called it in main class

---
+++ src/menuDrivenProgram/Pattern/Main.java
@@ -0,0 +1,8 @@
+package menuDrivenProgram.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Patterns patterns = new Patterns();
+        patterns.RightPattern(5);
+    }
+}
---
+++ src/menuDrivenProgram/Pattern/Patterns.java
@@ -0,0 +1,18 @@
+package menuDrivenProgram.Pattern;
+
+public class Patterns {
+    public void RightPattern(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) {
+
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+}
